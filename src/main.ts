cba1b92f31c8a639c62812627bcaa225bc9bfc16
André Teixeira <andre-x8@hotmail.com>
2023-08-22 12:48:08 -0300

    comentario

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,15 @@ bootstrap();
 
 // Projeto crud basico com base no curso do youyube
 //https://www.youtube.com/watch?v=tfsa_NyJfrA&list=PL4bT56Uw3S4zIAOX1ddx8oJS6jaD43vZC&index=2
+
+/**ATENÇÃO!!! 
+ * AO executar p projeto a  primeira vez pode dar um erro de conexão do mysql 
+ * o nome do erro é 
+ *  [Nest] 17832  - 22/08/2023, 12:38:03   ERROR [TypeOrmModule] Unable to connect to the database. Retrying (1)...
+    Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol
+    
+    para corrigir basta executar o seguinte comando no seu mysql :
+    
+    ALTER USER 'seu_usuario'@'seu_host' IDENTIFIED WITH 'mysql_native_password' BY 'sua_senha';
+
+ */
