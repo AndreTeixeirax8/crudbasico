86bb48384049841954f17d9e98ee1db8472bc439
André Teixeira <andre-x8@hotmail.com>
2023-08-22 12:13:05 -0300

    criando o .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { SERVER_PORT } from './config/constants';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService =app.get(ConfigService);//porta do servidor nest
+  const port = +configService.get<number>(SERVER_PORT)
+  await app.listen(port);
+  console.log(`Servidor rodando na porta ${await app.getUrl()} ` )
 }
 bootstrap();
+
+// Projeto crud basico com base no curso do youyube
+//https://www.youtube.com/watch?v=tfsa_NyJfrA&list=PL4bT56Uw3S4zIAOX1ddx8oJS6jaD43vZC&index=2
